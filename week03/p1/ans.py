0813a0503e951a9db1ac67866ca4e5ca90c1c9a7
juice500ml <juice500ml@gmail.com>
2016-03-21 17:29:56 +0900

    new problem sets~

---
+++ week03/p1/ans.py
@@ -0,0 +1,10 @@
+a = int(input('국어 성적을 입력하세요 : '))
+b = int(input('수학 성적을 입력하세요 : '))
+c = int(input('영어 성적을 입력하세요 : '))
+
+print('국어 성적 :', a)
+print('수학 성적 :', b)
+print('영어 성적 :', c)
+print('\n-----------------\n')
+print('총점 :', a+b+c)
+print('평균 :',(a+b+c)/3)
